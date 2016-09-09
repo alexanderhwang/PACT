22717f504ebfc6ba7c65eac7a02b99bcd27d61e6
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-09 16:58:17 -0700

    Create Fae.java

---
+++ Fae.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+
+public class Fae {
+	String name;
+	String faeClass;
+	ArrayList<String> givenNameArray;
+	ArrayList<String> attributeArray;
+	ArrayList<String> skillArray; //make skill class
+	int typePhysical;
+	int typeWeapon;
+	int typeSpell;
+	int typePsychic;
+	int typeHoly;
+	int typeEvil;
+	int elementNeutral;
+	int elementEarth;
+	int elementAir;
+	int elementWater;
+	int elementFire;
+	int elementIce;
+	int elementPower;
+	int elementForce;
+	int elementWood;
+	int elementPoison;
+	int elementMetal;
+	int elementBone;
+	int elementMind;
+	int elementSpirit;
+	int elementLight;
+	int elementDarkness;
+	int elementArcanum;
+	int elementHeaven;
+	int elementHell;
+	int elementChaos;
+	int elementAlmighty;
+	int elementVoid;
+	int statusSleep;
+	//etc. status
+	//storage for items/mana
+	//experience worth
+}
