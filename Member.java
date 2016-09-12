bf33c02d09151a73f0380169174e49fa6722b3b9
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-11 18:38:49 -0700

    Create Member.java

---
+++ Member.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Member {
+	Fae fae;
+	String chosenName;
+	String variety;
+	String rank;
+	//ArrayList<Aspect> aspectArray;
+	//ArrayList<Skill> skillArray;
+	
+	int level;
+	int vitality; int wisdom; int strength; int intelligence; int stamina; int dexterity; int passion; int resolve; int agility; int luck;
+	int health; int currentHealth; int energy; int currentEnergy;
+	int attack; int focus; int defense; int resistance; int tolerance; int accuracy; int evasion; int speed; int criticalRate; int castRate;
+	
+	ArrayList<Integer> attributes = new ArrayList<Integer>(Arrays.asList(vitality, wisdom, strength, intelligence, stamina, dexterity, passion, resolve, agility, luck));
+	ArrayList<Integer> hiddenAttributes = new ArrayList<Integer>(Arrays.asList(attack, focus, defense, resistance, tolerance, accuracy, evasion, speed, criticalRate, castRate));
+	
+	public Member(Fae fae, String chosenName, String variety, String rank) {
+		this.fae = fae;
+		this.chosenName = chosenName;
+		this.variety = variety;
+		this.rank = rank;
+	}
+}
