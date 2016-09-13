881f7762f5ab15077769102fbfa480acb8aa39d1
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-13 10:25:03 -0700

    Create SpeechBox.java

---
+++ SpeechBox.java
@@ -0,0 +1,26 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.GeneralPath;
+
+import javax.swing.JLabel;
+
+
+public class SpeechBox extends JLabel{
+	private GeneralPath path;
+	
+	public SpeechBox(GeneralPath path) {
+		this.path = path;
+	}
+	
+	protected void paintComponent(final Graphics g) {
+		final Graphics2D graphics2D = (Graphics2D) g;
+		graphics2D.setPaint(new Color(252, 252, 252, 234));
+		graphics2D.fill(path);
+		graphics2D.setPaint(new Color(3, 3, 3));
+		graphics2D.setStroke(new BasicStroke(2));
+		graphics2D.draw(path);
+	}
+
+}
