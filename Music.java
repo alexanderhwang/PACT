edb443f662c77cac40f92abcaaef0607f45f8067
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-14 17:26:08 -0700

    Create Music.java

---
+++ Music.java
@@ -0,0 +1,39 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.swing.SwingUtilities;
+
+public class Music {
+	static File soundFile;
+	static Clip clip;
+
+	public static void play(String sf) throws Exception{
+		soundFile = new File(sf);
+		clip = AudioSystem.getClip();
+		AudioInputStream stream = AudioSystem.getAudioInputStream(soundFile);
+        clip.open(stream);
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	
+            }
+        });
+	}
+	
+	public static void pause() {
+		clip.stop();
+	}
+	
+	public static void resume() {
+		clip.start();
+	}
+	
+	public static void changeVolume(float value) {
+		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+		gainControl.setValue(value);
+	}
+}
