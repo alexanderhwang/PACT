94fe4ae6c74f50310bc4a1d1e5bbf7567731258f
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-20 10:08:38 -0700

    Create MegaMouseAdapter.java

---
+++ MegaMouseAdapter.java
@@ -0,0 +1,14 @@
+import java.awt.event.MouseAdapter;
+
+
+public class MegaMouseAdapter extends MouseAdapter{
+	private int savedValue;
+
+	public MegaMouseAdapter(int savedValue) {
+		this.savedValue = savedValue;
+	}
+	
+	public int getSavedValue() {
+		return savedValue;
+	}
+}
