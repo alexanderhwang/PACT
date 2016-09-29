7662b81a4133ac14f698f741aa766d19330f7c15
Alexander Hwang <kuteragenesis@yahoo.com>
2016-09-28 21:11:55 -0700

    Update Fae.java

--- Fae.java
+++ Fae.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+
 
 public class Fae {
 	String name;
 	String faeClass;
 	ArrayList<String> givenNameArray;
-	ArrayList<String> attributeArray;
-	ArrayList<String> skillArray; //make skill class
-	int typePhysical;
-	int typeWeapon;
-	int typeSpell;
+	ArrayList<String> varietyArray;
+	ArrayList<String> rankArray;
+	ArrayList<String> flagArray;
+	ArrayList<Integer> initialAttributeArray; //vitality, wisdom, strength, intelligence, stamina, dexterity, passion, resolve, agility, luck
+	ArrayList<Integer> initialHiddenAttributeArray; //health, energy, attack, focus, defense, resistance, tolerance, accuracy, evasion, speed, critical rate, cast rate
+	//ArrayList<PossibleAspect> possibleAspectArray; //Aspect, condition -- maybe none?
+	//ArrayList<PossibleSkill> possibleSkillArray; //Aspect, condition -- maybe none?
+	//ArrayList<Transformation> transformationArray; //Fae, conditions
+	//LevelClass levelClass;
+	int typeImpact;
+	int typeCutting;
+	int typePiercing;
+	int typeMagic;
 	int typePsychic;
 	int typeHoly;
 	int typeEvil;
@@ -35,8 +45,32 @@ public class Fae {
 	int elementChaos;
 	int elementAlmighty;
 	int elementVoid;
-	int statusSleep;
-	//etc. status
-	//storage for items/mana
-	//experience worth
+	int ailmentDeath;
+	int ailmentWound;
+	int ailmentPetrification;
+	int ailmentDampness;
+	int ailmentBurn;
+	int ailmentFreeze;
+	int ailmentShock;
+	int ailmentPressure;
+	int ailmentPoisoning;
+	int ailmentConfusion;
+	int ailmentSilence;
+	int ailmentSleep;
+	int manaStorage;
+	int experienceMultiplier;
+	//String spriteBase;
+	
+	ImageIcon frontSprite;
+	ImageIcon backSprite;
+	
+	public Fae(String name) { //Basic fae creation, randomized
+		this.name = name;
+		setData();
+	}
+	
+	public void setData() {
+		//TODO set all init data
+		
+	}
 }
