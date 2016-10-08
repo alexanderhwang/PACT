bafe767fe5223816446401972cfe253f35d16112
Alexander Hwang <kuteragenesis@yahoo.com>
2016-10-07 17:31:54 -0700

    Update Fae.java

--- Fae.java
+++ Fae.java
@@ -18,12 +18,14 @@ public class Fae {
 	ArrayList<String> varietyArray; //manifest as sex+type (when applicable) separately, check for bracketed values to add to flags
 	ArrayList<String> possibleRankArray; //rank+condition
 	ArrayList<String> flagArray;
+	ArrayList<Integer> initialPreferenceArray; //vitality, wisdom, strength, intelligence, stamina, dexterity, passion, resolve, agility, luck
 	ArrayList<Integer> initialAttributeArray; //vitality, wisdom, strength, intelligence, stamina, dexterity, passion, resolve, agility, luck
 	ArrayList<Integer> initialBasicAttributeArray; //health, energy, attack, focus, defense, resistance, tolerance, accuracy, evasion, speed, critical rate, cast rate
 	ArrayList<String> possibleAspectArray;
 	ArrayList<String> possibleSkillArray; //Aspect+condition -- maybe none?
 	ArrayList<String> possibleTransformationArray; //Fae, conditions
-	String levelClass;
+	int levelClass; //0 - 8: fast, medium to fast, fast to medium, medium, slow to fast, fast to slow, slow to medium, medium to slow, slow
+	int rankOffset;
 	int typeImpact;
 	int typeCutting;
 	int typePiercing;
@@ -31,6 +33,7 @@ public class Fae {
 	int typePsychic;
 	int typeHoly;
 	int typeEvil;
+	ArrayList<Integer> typeArray = new ArrayList<Integer>(Arrays.asList(typeImpact, typeCutting, typePiercing, typeMagic, typePsychic, typeHoly, typeEvil));
 	int elementNeutral;
 	int elementEarth;
 	int elementAir;
@@ -53,6 +56,8 @@ public class Fae {
 	int elementChaos;
 	int elementAlmighty;
 	int elementVoid;
+	ArrayList<Integer> elementArray = new ArrayList<Integer>(Arrays.asList(elementNeutral, elementEarth, elementAir, elementWater, elementFire, elementIce, elementPower, elementForce, elementWood, elementPoison,
+			elementMetal, elementBone, elementMind, elementSpirit, elementLight, elementDarkness, elementArcanum, elementHeaven, elementHell, elementChaos, elementAlmighty, elementVoid));
 	int ailmentDeath;
 	int ailmentWound;
 	int ailmentPetrification;
@@ -65,6 +70,8 @@ public class Fae {
 	int ailmentConfusion;
 	int ailmentSilence;
 	int ailmentSleep;
+	ArrayList<Integer> ailmentArray = new ArrayList<Integer>(Arrays.asList(ailmentDeath, ailmentWound, ailmentPetrification, ailmentDampness, ailmentBurn, ailmentFreeze, ailmentShock,
+			ailmentPressure, ailmentPoisoning, ailmentConfusion, ailmentSilence, ailmentSleep));
 	int spirit;
 	int experienceMultiplier;
 	ArrayList<String> spriteBaseArray;
@@ -128,25 +135,73 @@ public class Fae {
     					break;
     				case 6:
     					ArrayList<String> tempArray1 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
-    					initialAttributeArray = new ArrayList<Integer>();
+    					initialPreferenceArray = new ArrayList<Integer>();
     					for (String element : tempArray1) {
-    						initialAttributeArray.add((int) Integer.parseInt(element));
+    						initialPreferenceArray.add((int) Integer.parseInt(element));
     					}
     					break;
     				case 7:
     					ArrayList<String> tempArray2 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
-    					initialBasicAttributeArray = new ArrayList<Integer>();
+    					initialAttributeArray = new ArrayList<Integer>();
     					for (String element : tempArray2) {
-    						initialBasicAttributeArray.add((int) Integer.parseInt(element));
+    						initialAttributeArray.add((int) Integer.parseInt(element));
     					}
     					break;
     				case 8:
+    					ArrayList<String> tempArray3 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					initialBasicAttributeArray = new ArrayList<Integer>();
+    					for (String element : tempArray3) {
+    						initialBasicAttributeArray.add((int) Integer.parseInt(element));
+    					}
+    					break;
+    				case 9:
     					possibleAspectArray = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
     					break;
-    					//TODO finish
+    				case 10:
+    					possibleSkillArray = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					break;
+    				case 11:
+    					possibleTransformationArray = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					break;
+    				case 12:
+    					levelClass = Integer.parseInt(dataString);
+    					break;
+    				case 13:
+    					rankOffset = Integer.parseInt(dataString);
+    					break;
+    				case 14:
+    					ArrayList<String> tempArray4 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					typeArray = new ArrayList<Integer>();
+    					for (String element : tempArray4) {
+    						typeArray.add((int) Integer.parseInt(element));
+    					}
+    					break;
+    				case 15:
+    					ArrayList<String> tempArray5 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					elementArray = new ArrayList<Integer>();
+    					for (String element : tempArray5) {
+    						elementArray.add((int) Integer.parseInt(element));
+    					}
+    					break;
+    				case 16:
+    					ArrayList<String> tempArray6 = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					ailmentArray = new ArrayList<Integer>();
+    					for (String element : tempArray6) {
+    						ailmentArray.add((int) Integer.parseInt(element));
+    					}
+    					break;
+    				case 17:
+    					spirit = Integer.parseInt(dataString);
+    					break;
+    				case 18:
+    					experienceMultiplier = Integer.parseInt(dataString);
+    					break;
+    				case 19:
+    					spriteBaseArray = new ArrayList<String>(Arrays.asList(dataString.split("\\s*,\\s*")));
+    					break;
     				}
     				line = line.substring(dataString.length() + 2, line.length());
-    				//TODO put string into value, update line
+    				//TODO check completion
     				progress++;
     			}
     		}
